bd8bcade9cec73bc70f686b89ece901c57b11199
nikitagood04 <149172772+nikitagood04@users.noreply.github.com>
2023-10-28 08:25:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
+
+burger.addEventListener('click', function () {
+    menu.classList.toggle('active')
+})
